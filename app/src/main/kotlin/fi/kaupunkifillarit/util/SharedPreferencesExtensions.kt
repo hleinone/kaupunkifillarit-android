97bc55ef185cdcbab7ce24279e79c04491478b9f
Hannu Leinonen <hleinone@gmail.com>
2016-08-12 10:20:52 +0300

    SharedPreferences observables that work.

--- app/src/main/kotlin/fi/kaupunkifillarit/util/SharedPreferencesExtensions.kt
+++ app/src/main/kotlin/fi/kaupunkifillarit/util/SharedPreferencesExtensions.kt
@@ -4,19 +4,19 @@ import android.content.SharedPreferences
 import com.bluelinelabs.logansquare.LoganSquare
 import com.bluelinelabs.logansquare.ParameterizedType
 import rx.Observable
-import rx.lang.kotlin.observable
+import rx.schedulers.Schedulers
 
-fun SharedPreferences.rx_getBoolean(key: String, defValue: Boolean): Observable<String> = observable { getBoolean(key, defValue) }
+fun SharedPreferences.rx_getBoolean(key: String, defValue: Boolean): Observable<Boolean> = Observable.just(getBoolean(key, defValue))
 
-fun SharedPreferences.rx_getFloat(key: String, defValue: Float): Observable<String> = observable { getFloat(key, defValue) }
+fun SharedPreferences.rx_getFloat(key: String, defValue: Float): Observable<Float> = Observable.just(getFloat(key, defValue))
 
-fun SharedPreferences.rx_getInt(key: String, defValue: Int): Observable<String> = observable { getInt(key, defValue) }
+fun SharedPreferences.rx_getInt(key: String, defValue: Int): Observable<Int> = Observable.just(getInt(key, defValue))
 
-fun SharedPreferences.rx_getLong(key: String, defValue: Long): Observable<String> = observable { getLong(key, defValue) }
+fun SharedPreferences.rx_getLong(key: String, defValue: Long): Observable<Long> = Observable.just(getLong(key, defValue))
 
-fun SharedPreferences.rx_getString(key: String, defValue: String): Observable<String> = observable { getString(key, defValue) }
+fun SharedPreferences.rx_getString(key: String, defValue: String): Observable<String> = Observable.just(getString(key, defValue))
 
-fun SharedPreferences.rx_getStringSet(key: String, defValue: Set<String>): Observable<String> = observable { getStringSet(key, defValue) }
+fun SharedPreferences.rx_getStringSet(key: String, defValue: Set<String>): Observable<Set<String>> = Observable.just(getStringSet(key, defValue))
 
 fun <T : Any> SharedPreferences.rx_object(key: String, type: Class<T>) = rx_getString(key, "").map { LoganSquare.parse(it, type) }
 
