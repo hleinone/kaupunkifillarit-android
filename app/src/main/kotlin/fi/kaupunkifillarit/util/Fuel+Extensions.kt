69d0b49aaef4afc9be772f700bd685962f914f48
Hannu Leinonen <hleinone@gmail.com>
2018-06-01 15:44:27 +0300

    Increase logging to find bugs in JSON deserialization.

--- app/src/main/kotlin/fi/kaupunkifillarit/util/Fuel+Extensions.kt
+++ app/src/main/kotlin/fi/kaupunkifillarit/util/Fuel+Extensions.kt
@@ -1,10 +1,11 @@
 package fi.kaupunkifillarit.util
 
+import com.crashlytics.android.Crashlytics
 import com.github.kittinunf.fuel.core.Request
 import com.github.kittinunf.fuel.core.ResponseDeserializable
 import com.github.kittinunf.fuel.rx.rx_object
 import kotlinx.serialization.KSerializer
-import kotlinx.serialization.MissingFieldException
+import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.JSON
 
 fun <T : Any> Request.rx_object(deserializer: KSerializer<T>) =
@@ -12,9 +13,14 @@ fun <T : Any> Request.rx_object(deserializer: KSerializer<T>) =
             override fun deserialize(content: String): T? {
                 return try {
                     JSON.nonstrict.parse(deserializer, content)
-                } catch (e: MissingFieldException) {
+                } catch (e: SerializationException) {
+                    Crashlytics.log("SerializationException in parsing JSON:\n$content")
+                    Crashlytics.logException(e)
+                    null
+                } catch (e: IllegalArgumentException) {
+                    Crashlytics.log("IllegalArgumentException in parsing JSON:\n$content")
+                    Crashlytics.logException(e)
                     null
                 }
             }
-        })
-
+        })  
