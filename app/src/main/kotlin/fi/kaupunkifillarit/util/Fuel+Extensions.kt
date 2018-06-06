dcee5b44b3fb3b8c9a5435b39f2565448ff85db9
Hannu Leinonen <hleinone@gmail.com>
2018-06-06 18:53:52 +0300

    Try to fix app crashing by increasing logging.

--- app/src/main/kotlin/fi/kaupunkifillarit/util/Fuel+Extensions.kt
+++ app/src/main/kotlin/fi/kaupunkifillarit/util/Fuel+Extensions.kt
@@ -4,23 +4,23 @@ import com.crashlytics.android.Crashlytics
 import com.github.kittinunf.fuel.core.Request
 import com.github.kittinunf.fuel.core.ResponseDeserializable
 import com.github.kittinunf.fuel.rx.rx_object
-import kotlinx.serialization.KSerializer
+import kotlinx.serialization.KSerialLoader
 import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.JSON
 
-fun <T : Any> Request.rx_object(deserializer: KSerializer<T>) =
+fun <T : Any> Request.rx_object(deserializer: KSerialLoader<T>) =
         rx_object(object : ResponseDeserializable<T> {
-            override fun deserialize(content: String): T? {
+            override fun deserialize(content: String): T {
                 return try {
                     JSON.nonstrict.parse(deserializer, content)
                 } catch (e: SerializationException) {
-                    Crashlytics.log("SerializationException in parsing JSON:\n$content")
-                    Crashlytics.logException(e)
-                    null
+                    Crashlytics.log("SerializationException in parsing JSON:")
+                    Crashlytics.setString("JSON", content)
+                    throw e
                 } catch (e: IllegalArgumentException) {
-                    Crashlytics.log("IllegalArgumentException in parsing JSON:\n$content")
-                    Crashlytics.logException(e)
-                    null
+                    Crashlytics.log("IllegalArgumentException in parsing JSON")
+                    Crashlytics.setString("JSON", content)
+                    throw e
                 }
             }
-        })  
+        })
